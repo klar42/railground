0426a799b35ea2501b241b25077c6371722e6d52
Tomas Fischer <tofische@users.noreply.github.com>
2017-11-25 21:58:10 +0100

    Speed up by supressing debug logging.

--- rg-model/cucumber.sh
+++ rg-model/cucumber.sh
@@ -1,1 +1,1 @@
-groovy -cp "$CUCUMBER_JVM_HOME/*:$PROB_HOME/lib/*" -e 'this.class.classLoader.loadClass("cucumber.api.cli.Main").main(args)' --glue features/step_definitions features/
+groovy -cp ".:$CUCUMBER_JVM_HOME/*:$PROB_HOME/lib/*" -e 'this.class.classLoader.loadClass("cucumber.api.cli.Main").main(args)' --glue features/step_definitions features/
